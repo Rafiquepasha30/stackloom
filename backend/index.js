c2fab7faf1a87a430cff0fadabc127e92ac029f5
Rafiquepasha30 <rafiquepasha30@gmail.com>
2025-06-17 18:42:30 +0530

    backend updated

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Backend is running successfully!');
+});
+
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
